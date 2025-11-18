b9dea3063b40d1e0889c44a108fd1f82e60838dd
marammiadi <maram.miadi12@gmail.com>
2025-11-18 09:54:36 +0100

    Ajout de la fonction acces_netmiko

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print'(Hello , Git)
+from netmiko import ConnectHandler
+import datetime
+
+def acces_netmiko():
+    Cisco_device = {
+            "Login": "admin",
+            "Pass": "C1sco12345",
+            "ssh port": "22",
+            "host": "sandbox-iosxr-1.cisco.com",
+                   }
+ssh = ConnectHandler(**cisco_device)
+affiche_interfaces = ssh.send_command("show ip interfaces brief")
+print(affiche_interfaces)
+
+
+x = datetime.datetime.now()
+print(x)
+
+
+
+
+
+
+print("Hello , Git!")
